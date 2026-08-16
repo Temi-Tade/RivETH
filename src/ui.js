69596a513118f694aca32bea5958aa7588a51e84
anirudhkhemriyaa <anirudhakhemriya06@gmail.com>
2026-08-16 14:35:28 +0530

    Update UI design and layout

--- src/ui.js
+++ src/ui.js
@@ -1,54 +1,54 @@
-function COPY_ADDRESS(btn) {
-    navigator.clipboard.writeText(btn.previousElementSibling.value)
-    .then(() => {
-        btn.style.display = "none";
-        btn.nextElementSibling.style.display = "block"
-        setTimeout(() => {
-            btn.style.display = "block";
-            btn.nextElementSibling.style.display = "none"
-        }, 2000);
-    })
-}
-
-function CREATE_MODAL(content) {
-    // todo - animate
-    document.querySelector("#modalbg").style.display = "block";
-    document.querySelector("#modal p").innerHTML = content;
-
-    document.querySelector("#modal button").onclick = function() {
-        document.querySelector("#modalbg").style.display = "none";
+    function COPY_ADDRESS(btn) {
+        navigator.clipboard.writeText(btn.previousElementSibling.value)
+        .then(() => {
+            btn.style.display = "none";
+            btn.nextElementSibling.style.display = "block"
+            setTimeout(() => {
+                btn.style.display = "block";
+                btn.nextElementSibling.style.display = "none"
+            }, 2000);
+        })
     }
 
-    window.onclick = function (e) {
-        if (e.target === document.querySelector("#modalbg")) {
+    function CREATE_MODAL(content) {
+        // todo - animate
+        document.querySelector("#modalbg").style.display = "block";
+        document.querySelector("#modal p").innerHTML = content;
+
+        document.querySelector("#modal button").onclick = function() {
             document.querySelector("#modalbg").style.display = "none";
         }
+
+        window.onclick = function (e) {
+            if (e.target === document.querySelector("#modalbg")) {
+                document.querySelector("#modalbg").style.display = "none";
+            }
+        }
     }
-}
 
-function DONATE() {
-    CREATE_MODAL(`
-        <div id="donate"><br/>
-            <h4>Thank you for supporting RivETH, together, we can build a community of web3 enthusiasts who are serious about security and growth!</h4>
-            <p>Donations can be sent to: 0x49Df3350fafa751212cCa8AB631cEa6e6ACB2ce6 (ERC20)</p>
-        </div>
-    `);
-}
-
-function TOGGLE_THEME(e) {
-    let sessionData = JSON.parse(localStorage.getItem("RivETHSessionData"));
-
-    if (e.checked) {
-        document.querySelector("#csslink").href = "./src/darkmode.css";
-        localStorage.setItem("RivETHSessionData", JSON.stringify({
-            ... sessionData,
-            theme: "dark"
-        }));
-    } else {
-        document.querySelector("#csslink").href = "";
-        localStorage.setItem("RivETHSessionData", JSON.stringify({
-            ... sessionData,
-            theme: "light"
-        }));
+    function DONATE() {
+        CREATE_MODAL(`
+            <div id="donate"><br/>
+                <h4>Thank you for supporting RivETH, together, we can build a community of web3 enthusiasts who are serious about security and growth!</h4>
+                <p>Donations can be sent to: 0x49Df3350fafa751212cCa8AB631cEa6e6ACB2ce6 (ERC20)</p>
+            </div>
+        `);
     }
-}
+
+    function TOGGLE_THEME(e) {
+        let sessionData = JSON.parse(localStorage.getItem("RivETHSessionData"));
+
+        if (e.checked) {
+            document.querySelector("#csslink").href = "./src/darkmode.css";
+            localStorage.setItem("RivETHSessionData", JSON.stringify({
+                ... sessionData,
+                theme: "dark"
+            }));
+        } else {
+            document.querySelector("#csslink").href = "";
+            localStorage.setItem("RivETHSessionData", JSON.stringify({
+                ... sessionData,
+                theme: "light"
+            }));
+        }
+    }
